b794328713d268bc096408365a74260f0aa45eb4
Sonadi <144614600+Sonadi@users.noreply.github.com>
2026-03-26 02:32:32 +0530

    frontend

---
+++ api.py
@@ -0,0 +1,49 @@
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI
+import joblib
+import numpy as np
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Load model and scaler
+model = joblib.load("heart_model.pkl")
+scaler = joblib.load("scaler.pkl")
+
+@app.get("/")
+def home():
+    return {"message": "Heart Disease Prediction API"}
+
+@app.post("/predict")
+def predict(data: dict):
+    features = [
+        data["age"],
+        data["sex"],
+        data["cp"],
+        data["trestbps"],
+        data["chol"],
+        data["fbs"],
+        data["restecg"],
+        data["thalach"],
+        data["exang"],
+        data["oldpeak"],
+        data["slope"],
+        data["ca"],
+        data["thal"]
+    ]
+
+    values = np.array(features).reshape(1, -1)
+    values = scaler.transform(values)
+
+    prediction = model.predict(values)[0]
+
+    return {
+        "prediction": int(prediction),
+        "result": "Heart Disease" if prediction == 1 else "No Heart Disease"
+    }
